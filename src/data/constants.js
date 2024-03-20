b2f3ed485587c0688d3705a3f6ae26c24aeb40ea
vignesh3022 <130534872+vignesh3022@users.noreply.github.com>
2024-03-20 14:09:16 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -15,7 +15,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/vignesh3022",
     resume:
-      "https://drive.google.com/file/d/1E2JX2emy1-nUKIcaIo8lv-kQPBs0ZU5p/view?usp=drivesdk",
+      "https://drive.google.com/file/d/1EBp3-xjWzaK4Vi9ku0JH-kyuKJ-J71CZ/view?usp=drivesdk",
     linkedin: "https://www.linkedin.com/in/vignesh-thipparthi-23197824a",
     Leetcode: "https://leetcode.com/VIGNESH_12B2/",
     insta: "https://www.instagram.com/t.vignesh_3004?igsh=MXd3OHkyczV5N25kdQ==",
