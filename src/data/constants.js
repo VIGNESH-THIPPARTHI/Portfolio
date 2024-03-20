f89162598c0b3303474db4348715cbb8dcb109ac
VIGNESH <20211a12b2@bvrit.ac.in>
2024-03-20 13:51:37 +0530

    Your commit message here

---
+++ src/data/constants.js
@@ -0,0 +1,276 @@
+import Deep from '../images/DeepfakeDetection.jpg';
+import Todos from '../images/Todos.png';
+import Weather from '../images/Weather.png';
+import quiz from '../images/Quizapp.png';
+export const Bio = {
+    name: "VIGNESH THIPPARTHI",
+    roles: [
+      "Full Stack Enthusiast",
+      "Programmer",
+      "UI/UX Developer",
+      "Cyber Security Enthusiast",
+
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/vignesh3022",
+    resume:
+      "https://drive.google.com/file/d/1E2JX2emy1-nUKIcaIo8lv-kQPBs0ZU5p/view?usp=drivesdk",
+    linkedin: "https://www.linkedin.com/in/vignesh-thipparthi-23197824a",
+    Leetcode: "https://leetcode.com/VIGNESH_12B2/",
+    insta: "https://www.instagram.com/t.vignesh_3004?igsh=MXd3OHkyczV5N25kdQ==",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+        
+    
+        
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Express Js",
+          image: "https://img.icons8.com/?size=128&id=2ZOaTclOqD4q&format=png",
+        },
+        {
+          name: "MongoDB",
+          image: "https://img.icons8.com/?size=96&id=74402&format=png",
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+      ],
+    },
+    
+    {
+      title: "Additional",
+      skills: [
+        {
+          name: "Linux",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRdKovOa8lVKIjdmfwrvfp_VEnlCZu41jaDd6uvg00mw&s",
+        },
+        {
+          name: "Python Programming",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "Java Programming",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name:"OOPS",
+          image:"https://res.cloudinary.com/practicaldev/image/fetch/s--ymOHw4aF--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/rewxsx85fqmolo6r49p9.jpg",
+        }
+       
+        
+        
+      ],
+    },
+    {
+      title: "Tools",
+      skills: [
+        {
+          name: "Kali Linux",
+          image:
+            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.2AV-e-3swfI-Nu9SbSanCAAAAA%26pid%3DApi&f=1&ipt=64c658e1453f716e6f1ab464374eeae0b6e5463acdb97b9c65ca1d3b215f0418&ipo=images",
+        },
+        {
+          name: "Git",
+          image:
+            "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://img.icons8.com/?size=96&id=9OGIyU8hrxW5&format=png",
+        },
+        {
+          name: "Jira",
+          image:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.jIuf4iEHhSBcX4x484UlOwHaEK%26pid%3DApi&f=1&ipt=dad3ee34098e2db35a96669243478c3d460a87e03dbfdf5a33981a158c5d67fe&ipo=images",
+
+        },
+        {
+          name: "Jupyter",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+        },
+        {
+          name: "Google Colab",
+          image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.laYIzRY2A-jpnBA2rO2jSQHaEj%26pid%3DApi&f=1&ipt=a91db9d15134be512d993ddb890716d355294fb5b46f6525e623f9980525298f&ipo=images",
+        },
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://gyaanarth.com/wp-content/uploads/2022/08/1167334_565884593486853_1203539694_o-1024x788.jpg",
+      school: "BV Raju Institute of Technology, Narsapur, Medak",
+      date: "Oct 2020 - Jun 2024",
+      grade: "8.86 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Information Technology and Engineering at BV Raju Institute Of Technology. I have completed 7 semesters and have a CGPA of 8.86. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Information Technology and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.logolynx.com%2Fimages%2Flogolynx%2F33%2F338f2c4e0d48a27ed6ad2a72821d3721.jpeg&f=1&nofb=1&ipt=780262f41ace27715c6b7c3f137f5e962569773c8e0f559da5167edcf00a5941&ipo=images",
+      school: "Narayana Junior College, Hyderabad",
+      date: "Jun 2017 - Apr 2019",
+      grade: "93.5%",
+      desc: "I completed my class 12 high school education at Narayana Junior College, Hyderabad, where I studied Maths, Physics and Chemistry.",
+      degree: "TSBIE(XII), MPC",
+    },
+    {
+      id: 2,
+      img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fis2-ssl.mzstatic.com%2Fimage%2Fthumb%2FPurple114%2Fv4%2Ff9%2Fde%2Fd6%2Ff9ded6cd-721e-d672-4e7c-a9a88a869281%2Fsource%2F512x512bb.jpg&f=1&nofb=1&ipt=ef2e0a2712947421592dc21f4218efc0957a6e0cb2b85f89a0b75b62153dd2b4&ipo=images",
+      school: "Krishnaveni High School, Metpally",
+      date: "Apr 2017",
+      grade: "97.0%",
+      desc: "I completed my class 10 education at Krishnaveni High School, Metpally.",
+      degree: "SSC(X)",
+    },
+  ];
+  
+  export const projects = [
+
+    {
+      id: 1,
+      title: "Todos Web App",
+      date: "Feb 2024",
+      description:
+        "This is a simple weather app built using React. It allows users to fetch weather information based on either the city name or their current location. The app utilizes the OpenWeatherMap API to retrieve weather data. It will fetch weather information based on city name or current location, display temperature in both Celsius and Fahrenheit, and show additional weather details such as humidity, wind speed, and weather description. Additionally, the background image changes based on weather conditions.",
+      image: Todos,
+      tags: ["HTML", "Javascript","CSS","Bootstrap","Anime.js","Local Storage" ],
+      category: "web app",
+      github: "https://github.com/vignesh3022/Todos",
+      webapp: "https://our-todos.netlify.app/",
+    },
+    
+    {
+      id: 2,
+      title: "Weather Web App",
+      date: "Mar 2024",
+      description:
+        "A React JS weather web app offering real-time weather data retrieval. Users can search by city or use automatic location detection. Integrated with the OpenWeatherMap API, it displays temperature in Celsius/Fahrenheit, humidity, and wind speed. The background image changes with weather conditions, enhancing visual appeal.",
+      image: Weather,
+      tags: ["React JS", "Axios", "Bootstrap", "Tailwind CSS","React Icons","React Toastify"],
+      category: "web app",
+      github: "https://github.com/vignesh3022/React-project-Weatherapp",
+      webapp: "https://our-weather-app.vercel.app/",
+
+    },
+    {
+      id: 4,
+      title: "Quiz Web App",
+      date: "Mar 2024",
+      description:
+        "An interactive quiz application built using React. Users can choose the number of questions they want to answer and start the quiz. It fetches questions from the Open Trivia Database API and presents them to the user. Users can select their answers and receive instant feedback. After completing the quiz, users can view their score and a summary of their answers.",
+      image:quiz,
+      tags: ["React JS", "JavaScript", "HTML", "CSS"],
+      category: "web app",
+      github: "https://github.com/vignesh3022/React-project-Quizapp",
+      webapp: "https://our-quizapp.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Smart Blind Stick",
+      date: "Sept 2020 - Oct 2022",
+      description:
+        "This innovative assistive technology, crafted for individuals with visual impairments, employs advanced ultrasonic sensors and the Arduino UNO platform. It provides precise obstacle detection within a 5-meter radius, accompanied by an audible buzzer signal and the added feature of creating vibrations. This dual-alert system enhances user safety and confidence in navigating their surroundings, offering a comprehensive solution for individuals with visual challenges.",
+      image:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F711An0gDgHL._SL1500_.jpg&f=1&nofb=1&ipt=a63a28e26bb2e3379e25a14c382530d403cb709a6b11c8a898da243c85df0eca&ipo=images",
+      tags: ["Arduino UNO", "Ultrasonic Sensor","Buzzer", "Code in C++"],
+      category: "Iot",
+    },
+    {
+      id: 4,
+      title: "Smart Wrist Band for Women",
+      date: "Oct 2020 - Nov 2022",
+      description:
+        "Engineered an IoT-powered Smart Wrist Band with an exclusive dedication to fortifying women's safety. This innovative device not only promptly dispatches certified alarm notifications but also facilitates real-time location sharing, thereby enhancing security during critical moments.The device constitute of Arduino Uno and a gsm module enabling to send messages and a geo locater that detects the user location and together helps to send the information.",
+      image:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcircuitdigest.com%2Fsites%2Fdefault%2Ffiles%2Fprojectimage_mic%2FWomen-Safety-Device.jpg&f=1&nofb=1&ipt=f2710e687c11ed135d2ba93448d21fac19768437618caeaf49ab8475907b4d33&ipo=images",
+      tags: ["Gsm", "Arduino","Geo locater"],
+      category: "Iot",
+    },
+    {
+      id: 5,
+      title: "Deep Fake Face Detection",
+      date: "Jan 2024 - Mar 2024",
+      description: 
+        ["Implemented a robust Deepfake Face Detection System using Long Short-Term Memory (LSTM) networks and Xception architecture. The project aimed to address the rising concerns related to deepfake technologies, specifically targeting facial manipulation. The combination of LSTM, known for its sequential data handling capabilities, and Xception, a powerful convolutional neural network (CNN), enabled accurate and real-time detection of manipulated facial images.",
+        "Utilized LSTM networks for handling sequential data.",
+        "Implemented the Xception architecture for powerful feature extraction.",
+        "Addressed concerns related to deepfake technologies.",
+        "Targeted facial manipulation scenarios."],
+      
+      image: Deep,
+      tags: ["LSTM", "Xception","TensorFlow", "Keras","OpenCV", "Precision", "Recall", "F1-score"],
+      category: "machine learning",
+    },
+  ];
+  
+  
