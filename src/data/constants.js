78f94067058c52555b2297f356df905470670964
vignesh3022 <130534872+vignesh3022@users.noreply.github.com>
2024-09-26 10:28:56 +0530

    updated resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -15,7 +15,7 @@ export const Bio = {
       "I am a driven and adaptable professional, consistently seeking out new opportunities for growth. Committed to excellence, I prioritize delivering exceptional outcomes. Possessing an optimistic outlook and a proactive approach, I am poised to bring valuable contributions and attain remarkable milestones.",
     github: "https://github.com/vignesh3022",
     resume:
-      "https://drive.google.com/file/d/1v9yTgYsbClZfe3jlOshaXcQ3_mO5WjV1/view?usp=drivesdk",
+      "https://drive.google.com/file/d/1-1p_t0BeU_Gj_2k2Po9hgY-W-sDqh4IJ/view?usp=drivesdk",
     linkedin: " https://www.linkedin.com/in/vignesh-thipparthi/",
     Leetcode: "https://leetcode.com/VIGNESH_12B2/",
     insta: "https://www.instagram.com/t.vignesh_3004?igsh=MXd3OHkyczV5N25kdQ==",
